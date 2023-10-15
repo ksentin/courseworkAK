2056e889e4dc19d8b1214753704da6ff95624966
ksentin <ksenpati@gmail.com>
2023-10-15 22:37:29 +0300

    Внесено зміни в calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -16,3 +16,11 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+double Calculator::Div(double a, double b)
+{
+    if (b != 0) {
+        return a / b;
+    } else {
+        return 0.0;
+    }
+}
